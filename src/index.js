95e18eb554231fb68e5432af98423e5b0897a61b
Chris Lin <lc1990linux@gmail.com>
2018-07-04 17:40:47 +0800

    [chen] introduce express and morgan

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 import http from 'http';
 
 import express from 'express';
-
-// let a = sum(3, 4);
-// console.log('a = ', a);
-
-// const callback = (err, method) => {
-//   if (err) {
-//     console.log(err.message);
-//   } else {
-//     console.log('area is ', method.area());
-//   }
-// };
-
-// let server = http.createServer((req, res) => {
-//   console.log(req);
-//   res.statusCode = 200;
-//   res.res.end(
-//     '<html> <title>title</title> <body> <h1>my header</h1> <p> the content </p> </body> </html>'
-//   );
-// });
+import morgan from 'morgan';
 
 const app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/../public'));
+console.log('__dirname = ', __dirname);
 
+//this is default response
 app.use((req, res) => {
-  console.log(req.headers);
+  // morgan will replace it to do log work.
+  // console.log(req.headers);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html> <title>hello</title> <body> <h1>title</h1> </body> </html>');
