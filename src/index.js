6a273c205d5fed56324eedbc1230d402f75b1275
Chris Lin <lc1990linux@gmail.com>
2018-07-04 14:02:36 +0800

    [chen] involve express

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
-import rect from "./rect";
-import sum from "./sum";
+import http from 'http';
 
-let a = sum(3, 4);
-console.log("a = ", a);
+import express from 'express';
 
-const callback = (err, method) => {
-  if (err) {
-    console.log(err.message);
-  } else {
-    console.log("area is ", method.area());
-  }
-};
-rect(4, 5, callback);
-console.log("after");
-rect(2, 3, callback);
-console.log("after");
-rect(0, 5, callback);
-console.log("after");
-rect(-1, 5, callback);
-console.log("after");
+// let a = sum(3, 4);
+// console.log('a = ', a);
+
+// const callback = (err, method) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     console.log('area is ', method.area());
+//   }
+// };
+
+// let server = http.createServer((req, res) => {
+//   console.log(req);
+//   res.statusCode = 200;
+//   res.res.end(
+//     '<html> <title>title</title> <body> <h1>my header</h1> <p> the content </p> </body> </html>'
+//   );
+// });
+
+const app = express();
+
+app.use((req, res) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html> <title>hello</title> <body> <h1>title</h1> </body> </html>');
+});
+
+const port = 3000;
+const hostName = 'localhost';
+const server = http.createServer(app);
+
+server.listen(port, hostName, () => {
+  console.log(`listen ${hostName}: ${port}`);
+});
